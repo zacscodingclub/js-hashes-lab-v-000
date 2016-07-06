8606662ace4b6a871b0770a0020de679b3cc57d7
zacscodingclub <zbaston@gmail.com>
2016-07-05 19:16:59 -0500

    Done.

--- recipe.js
+++ recipe.js
@@ -1,1 +1,26 @@
 'use strict';
+
+function addIngredient(recipe, ingredientName, ingredentAmount) {
+  recipe[ingredientName] = ingredentAmount;
+
+  return recipe;
+}
+
+function removeIngredient(recipe, ingredientName) {
+  delete recipe[ingredientName];
+
+  return recipe;
+}
+
+function updateIngredient(recipe, ingredientName, ingredientAmount) {
+  recipe[ingredientName] = ingredientAmount;
+
+  return recipe;
+}
+
+function readRecipe(recipe) {
+
+  for (var key in recipe) {
+    console.log("this recipe calls for " + recipe[key] + " of " + key);
+  }
+}
